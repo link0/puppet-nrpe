c38fcb5e10ef59c39de81996887f4518f8c083df
Dennis de Greef <github@link0.net>
2016-09-04 19:20:34 +0200

    Make sure ensure statements are first

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,8 @@
 class nrpe::install inherits nrpe {
   if $nrpe::package_manage {
     package { 'nrpe':
-      name   => $nrpe::package_name,
       ensure => $nrpe::package_ensure,
+      name   => $nrpe::package_name,
     }
   }
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,8 @@
 class nrpe::service inherits nrpe {
   if $nrpe::service_manage {
     service { 'nrpe':
-      name   => $nrpe::service_name,
       ensure => $nrpe::service_ensure,
+      name   => $nrpe::service_name,
     }
   }
 }
